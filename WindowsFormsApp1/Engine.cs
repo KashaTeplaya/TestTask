0da3feda6a252f91b64c434ca4a8ccc6d3191d1e
Андрей Бачанов <bachanow18@gmail.com>
2019-09-11 18:11:00 +0600

    version 1

---
+++ WindowsFormsApp1/Engine.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public class Engine
+    {
+        int i = 0;
+        Random random = new Random();
+        
+        public void Counter(Label label)
+        {
+            i++;
+            label.Text = i.ToString();
+        }
+        public void ColorChanger(Form form)
+        {
+            if (i % 2 == 0)
+            {
+               form.BackColor = Color.Blue;
+            }
+            else form.BackColor = Color.Red;
+            if (i % 3 == 0)
+            {
+                form.BackColor = Color.FromArgb(random.Next(255), random.Next(255), random.Next(255));
+            }
+        }
+    }
+}
---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(203, 150);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(370, 114);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Получишь результат";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Label2.Location = new System.Drawing.Point(370, 111);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(23, 25);
+            this.Label2.TabIndex = 2;
+            this.Label2.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "ColorChanger2000";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Button button1;
+        public System.Windows.Forms.Label Label2;
+    }
+}
+
